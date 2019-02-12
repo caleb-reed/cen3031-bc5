e6243c297f59fc17a60bc0cdf4f550f316a89310
Caleb Reed <calebreed@ufl.edu>
2019-02-12 02:15:30 -0500

    Updating URLs AGAIN

--- client/js/factories/listingFactory.js
+++ client/js/factories/listingFactory.js
@@ -1,18 +1,18 @@
 angular.module('listings', []).factory('Listings', function($http) {
   var methods = {
     getAll: function() {
-        return $http.get('https://bc5-calebr.herokuapp.com/api/listings');
+        return $http.get('https://bc5-cr.herokuapp.com/api/listings');
     },
 	
 	create: function(listing) {
-        return $http.post('https://bc5-calebr.herokuapp.com/api/listings', listing);
+        return $http.post('https://bc5-cr.herokuapp.com/api/listings', listing);
     }, 
 
     delete: function(id) {
 	   /**TODO
         return result of HTTP delete method
        */
-        return $http.delete('https://bc5-calebr.herokuapp.com/api/listings/' + id);
+        return $http.delete('https://bc5-cr.herokuapp.com/api/listings/' + id);
     }
   };
 
